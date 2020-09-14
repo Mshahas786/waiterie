f01182072edcfd8009ba26a6ef74c62be42f68a8
Mshahas786 <mshahas786@gmail.com>
2020-09-14 17:13:53 +1200

    filter menu done with some add to card options

--- js/script.js
+++ js/script.js
@@ -48,20 +48,55 @@ $(document).ready(function () {
 
 
 
-filterSelection("all")
+
+
+// var slideshow = 0;
+// showSlides();
+
+// function showSlides() {
+//   var i;
+//   var slides = document.getElementsByClassName("promotion_img");
+//   var dots = document.getElementsByClassName("dot");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";  
+//   }
+//   slideshow++;
+//   if (slideshow > slides.length) {slideshow = 1}    
+//   for (i = 0; i < dots.length; i++) {
+//     dots[i].className = dots[i].className.replace(" active", "");
+//   }
+//   slides[slideshow-1].style.display = "block";  
+//   dots[slideshow-1].className += " active";
+//   setTimeout(showSlides, 2000); // Change image every 2 seconds
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+filterSelection("allmenu")
 function filterSelection(c) {
   var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
+  x = document.getElementsByClassName("menu_item");
+  if (c == "allmenu") c = "";
   // Add the "show" class (display:block) to the filtered elements, and remove the "show" class from the elements that are not selected
   for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
+    RemoveClass(x[i], "show");
+    if (x[i].className.indexOf(c) > -1) AddClass(x[i], "show");
   }
 }
 
 // Show filtered elements
-function w3AddClass(element, name) {
+function AddClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -73,7 +108,7 @@ function w3AddClass(element, name) {
 }
 
 // Hide elements that are not selected
-function w3RemoveClass(element, name) {
+function RemoveClass(element, name) {
   var i, arr1, arr2;
   arr1 = element.className.split(" ");
   arr2 = name.split(" ");
@@ -86,7 +121,7 @@ function w3RemoveClass(element, name) {
 }
 
 // Add active class to the current control button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
+var btnContainer = document.getElementById("filter_Container");
 var btns = btnContainer.getElementsByClassName("btn");
 for (var i = 0; i < btns.length; i++) {
   btns[i].addEventListener("click", function() {
